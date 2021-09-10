b5ebb47d726f5f0ec9f2f39286921fde3105ac65
2019060164 <2019060164@hanyang.ac.kr>
2021-09-10 21:07:50 +0900

    boostcourse-study

---
+++ boostcourse/firstweb/src/main/java/examples/LifeCycleServlet.java
@@ -0,0 +1,72 @@
+package examples;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LifeCycleServlet
+ */
+@WebServlet("/LifeCycleServlet")
+public class LifeCycleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LifeCycleServlet() {
+        System.out.println("LifecycleServlet 생성!!");
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		System.out.println("init 호출!!");
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+		System.out.println("destroy 호출!!");
+	}
+
+
+	
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<head><title>form</title></head>");
+		out.println("<body>");
+		out.println("<form method='post' action='/firstweb/LifeCycleServlet'>");
+		out.println("name : <input type='text' name='name'><br>");
+		out.println("<input type='submit' value='ok'><br>");
+		out.println("</form>");
+		out.println("</body>");
+		out.println("</html>");
+		out.close();
+		
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+		String name = req.getParameter("name");
+		out.println("<h1> hello " + name + "</h1>");
+		out.close();
+	}
+
+}
---
+++ boostcourse/webapiexam/src/main/java/kr/or/connect/jdbcexam/dao/RoleDao.java
@@ -0,0 +1,47 @@
+package kr.or.connect.jdbcexam.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import kr.or.connect.jdbcexam.dto.Role;
+
+public class RoleDao {
+	private static String dburl = "jdbc:mysql://localhost:3306/connectdb";
+	private static String dbUser = "connectuser";
+	private static String dbpasswd = "connect123!@#";
+
+	public List<Role> getRoles() {
+		List<Role> list = new ArrayList<>();
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		String sql = "SELECT description, role_id FROM role order by role_id desc";
+		try (Connection conn = DriverManager.getConnection(dburl, dbUser, dbpasswd);
+				PreparedStatement ps = conn.prepareStatement(sql)) {
+
+			try (ResultSet rs = ps.executeQuery()) {
+
+				while (rs.next()) {
+					String description = rs.getString(1);
+					int id = rs.getInt("role_id");
+					Role role = new Role(id, description);
+					list.add(role); // list에 반복할때마다 Role인스턴스를 생성하여 list에 추가한다.
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return list;
+	}
+}
